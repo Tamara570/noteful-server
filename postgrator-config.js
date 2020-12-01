f5bb882d2d0314aa1f347b450770a8fec1fad33c
Tamara Walker <tamaralw115@gmail.com>
2020-12-01 08:16:13 -0500

    preparing for deployment

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,4 +7,23 @@ module.exports = {
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
+    "host": process.env.MIGRATION_DATABASE_HOST,
+    "port": process.env.MIGRATION_DATABASE_PORT,
+    "database": process.env.MIGRATION_DATABASE_NAME,
+    "username": process.env.MIGRATION_DATABASE_USER,
+    "password": process.env.MIGRATION_DATABASE_PASS
 }
+
+
+
+
+
+
+
+
+//         MIGRATION_DATABASE_HOST=localhost
+//         MIGRATION_DATABASE_PORT=5432
+//         MIGRATION_DATABASE_NAME=noteful
+//         MIGRATION_DATABASE_USER=postgres
+//         MIGRATION_DATABASE_PASS=
+// 
